9b8357c77efbf502f912edb5c6bf059c6fdce68f
sejjy <sejjymvm@gmail.com>
2025-09-09 22:52:23 +0800

    chore: initial commit

---
+++ init.lua
@@ -0,0 +1,14 @@
+require("full-border"):setup({
+	-- type = ui.Border.PLAIN,
+})
+
+th.git = th.git or {}
+th.git.modified_sign = " M"
+th.git.added_sign = " A"
+th.git.untracked_sign = "??"
+th.git.ignored_sign = "!!"
+th.git.deleted_sign = " D"
+th.git.updated_sign = " U"
+
+require("git"):setup()
+require("no-status"):setup()
